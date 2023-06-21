755df9101522737f3a3157242ad136104b7e181b
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-06-22 00:48:22 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include "monty.h"
 bus_t bus = {NULL, NULL, NULL, 0};
 /**
-* main - monty code interpreter
+* main : monty code interpreter
 * @argc: number of arguments
 * @argv: monty file location
-* Return: 0 on success
+* Return: 0  success
 */
 int main(int argc, char *argv[])
 {

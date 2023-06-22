60360ca3b2fe03fa6591dcbea1c3b645f25eb22d
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-06-23 00:25:28 +0100

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 
 /**
- * swap - swaps the top two elements of the stack
+ * op_swap - swaps the top two elements of the stack
  * @stack: pointer to the top of the stack
  * @line_number: line number of the opcode
 */
-void swap(stack_t **stack, unsigned int line_number)
+void op_swap(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 

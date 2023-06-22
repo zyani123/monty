f8103f787d06758b975c013c63e5f0279ab0cd26
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-06-23 00:40:27 +0100

    Update nop.c

--- nop.c
+++ nop.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 
 /**
- * nop - does nothing
+ * op_nop - does nothing
  * @stack: pointer to the top of the stack
  * @line_number: line number of the opcode
  **/
-void nop(stack_t **stack, unsigned int line_number)
+void op_nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
 	(void)line_number;

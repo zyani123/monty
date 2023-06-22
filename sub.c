ea5663ea2bd79f25d075ae45cbcc36b9081c9e1a
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-06-23 00:28:30 +0100

    Update sub.c

--- sub.c
+++ sub.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 
 /**
- * sub - subtracts the top element of the stack from the second top element
+ * op_sub - subtracts the top element of the stack from the second top element
  * @stack: double pointer to the head of the stack
  * @line_number: current line number in the file
  */
-void sub(stack_t **stack, unsigned int line_number)
+void op_sub(stack_t **stack, unsigned int line_number)
 {
 	int result;
 	stack_t *temp;

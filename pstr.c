67dcc38ba617bdbf84471017828b08af57b49818
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-06-23 00:11:55 +0100

    Update pstr.c

--- pstr.c
+++ pstr.c
@@ -1,12 +1,12 @@
 #include "monty.h"
 
 /**
- * op_pstr - prints the string starting at the top of the stack,
+ * pstr - prints the string starting at the top of the stack,
  *           followed by a new line
  * @stack: double pointer to the top of the stack.
  * @line_number: current line number of the opcode in the Monty file
  */
-void op_pstr(stack_t **stack, unsigned int line_number)
+void pstr(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = *stack;
 	(void) line_number;

59567ec055fc1ba30b6330719725b7b14982e009
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-06-23 00:31:39 +0100

    Update pop.c

--- pop.c
+++ pop.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 
 /**
- * pop - removes the top element of the stack
+ * op_pop - removes the top element of the stack
  * @stack: pointer to the top of the stack
- * @line_number: line number of the opcode
+ * @line_number: line number  the opcode
  **/
-void pop(stack_t **stack, unsigned int line_number)
+void op_pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 

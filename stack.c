ff609fd9997a01182e673b2ff3f9f4ffdecb50f5
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-06-23 02:07:52 +0100

    Update stack.c

--- stack.c
+++ stack.c
@@ -3,11 +3,10 @@
  * f_stack - prints the top
  * @head: stack head
  * @counter: line_number
- * Return: no return
 */
-void f_stack(stack_t **head, unsigned int counter)
+void f_stack(stack_t **stack, unsigned int counter)
 {
-	(void)head;
+	(void)stack;
 	(void)counter;
 	glob.lifi = 0;
 }

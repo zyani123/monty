4ea90a754945211fc163a6f75fca554bf87a9e32
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-06-23 01:26:42 +0100

    Update rotr.c

--- rotr.c
+++ rotr.c
@@ -1,20 +1,26 @@
 #include "monty.h"
 /**
- * rotr - "rotate right" put bottom of stack on top
- *
- * @top: top of stack
- * @bot: bottom of stack
+  *f_rotr- rotates the stack to the bottom
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
  */
-void rotr(stack_t **top, stack_t **bot)
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
 {
-	stack_t *ptrt = *top, *ptrb = *bot;
+	stack_t *copy;
 
-	if (ptrt == NULL || ptrt->prev == NULL)
+	copy = *head;
+	if (*head == NULL || (*head)->next == NULL)
+	{
 		return;
-	ptrt->next = ptrb;
-	ptrb->prev = ptrt;
-	*bot = ptrb->next;
-	(*bot)->prev = NULL;
-	*top = ptrb;
-	ptrb->next = NULL;
+	}
+	while (copy->next)
+	{
+		copy = copy->next;
+	}
+	copy->next = *head;
+	copy->prev->next = NULL;
+	copy->prev = NULL;
+	(*head)->prev = copy;
+	(*head) = copy;
 }
